71a4ec2a1404ad16ef3bb31f4ab4b3ea412d4746
王杰 <wangjie7@yhd.com>
2016-03-18 18:25:35 +0800

    no message

---
+++ JWUIKitDemo/JWUIKitDemo/AppDelegate.swift
@@ -0,0 +1,54 @@
+//
+//  AppDelegate.swift
+//  JWUIKitDemo
+//
+//  Created by Jerry on 16/3/17.
+//  Copyright © 2016年 Jerry Wong. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        
+        self.initAppreance()
+        
+        self.window = UIWindow(frame: UIScreen.mainScreen().bounds)
+        let widgetsListVC = WidgetsListViewController()
+        let navigationController = UINavigationController(rootViewController: widgetsListVC)
+        self.window?.rootViewController = navigationController
+        self.window?.makeKeyAndVisible()
+        
+        return true
+    }
+
+    func applicationWillResignActive(application: UIApplication) {
+        
+    }
+
+    func applicationDidEnterBackground(application: UIApplication) {
+        
+    }
+
+    func applicationWillEnterForeground(application: UIApplication) {
+        
+    }
+
+    func applicationDidBecomeActive(application: UIApplication) {
+        
+    }
+
+    func applicationWillTerminate(application: UIApplication) {
+        
+    }
+
+    func initAppreance(){
+        UINavigationBar.appearance().tintColor = UIColor(red: 249.0 / 255.0, green: 147.0 / 255.0, blue: 104.0 / 255.0, alpha: 1.0)
+    }
+}
+
---
+++ JWUIKitDemo/JWUIKitDemo/ViewController.swift
@@ -0,0 +1,47 @@
+//
+//  ViewController.swift
+//  JWUIKitDemo
+//
+//  Created by Jerry on 16/3/17.
+//  Copyright © 2016年 Jerry Wong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, JWPageViewDataSource, JWPageViewDelegate{
+    
+//MARK: - LifeCycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let pageView = JWPageView(frame: CGRectMake(0, 20, CGRectGetWidth(self.view.frame), 200))
+        pageView.delegate = self
+        pageView.dataSource = self
+        pageView.backgroundColor = UIColor.redColor()
+        self.view.addSubview(pageView)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+//MARK: - JWPageViewDataSource & JWPageViewDelegate
+
+    func numberOfPagesInPageView(aPageView: JWPageView) -> UInt {
+        return 4
+    }
+    
+    func pageView(aPageView: JWPageView, viewAt aIndex: UInt, var reusableView: UIView?) -> UIView{
+        if (reusableView == nil) {
+            let label = UILabel()
+            label.textColor = UIColor.whiteColor()
+            label.font = UIFont.systemFontOfSize(20.0)
+            label.textAlignment = .Center
+            reusableView = label
+        }
+        let label: UILabel = reusableView as! UILabel
+        label.text = "第\(aIndex)个页面"
+        return reusableView!
+    }
+}
+
